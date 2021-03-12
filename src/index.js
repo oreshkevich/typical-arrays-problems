3cb66531dd874ade3856af7b0a1d4ed1327c0ad7
Henadzi <68184678+oreshkevich@users.noreply.github.com>
2021-03-12 17:46:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined) {
+        return 0;
+    } else if (array.length) {
+        return Math.min.apply(null, array);
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+   if (array === undefined) {
+        return 0;
+    } else if (array.length) {
+        return Math.max.apply(null, array);
+    } else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+ if (array === undefined) {
+        return 0;
+    } else if (array.length) {
+        let total = 0;
+        for (let i = 0; i < array.length; i++) {
+            total += array[i];
+        }
+        let avg = total / array.length;
+        return avg;
+    } else {
+        return 0;
+    }
 }
